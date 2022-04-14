a80fb9c3470ffdcebd9b47bf5d15c352c353f9ab
minenice55 <43734252+minenice55@users.noreply.github.com>
2022-04-14 12:48:32 -0400

    API: add MiiStudio Api

--- Festival2048/app/src/main/java/com/example/festival2048/MainActivity.java
+++ Festival2048/app/src/main/java/com/example/festival2048/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.festival2048;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    MiiStudioApi testMii;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        testMii = new MiiStudioApi("080040030C040402020C0306060406020A0000000005000804000A0600374004000214031304170D04000A040109");
+        Log.println(Log.DEBUG, "mii url:", testMii.getMiiUrl());
     }
 }
---
+++ Festival2048/app/src/main/java/com/example/festival2048/MiiStudioApi.java
@@ -0,0 +1,83 @@
+package com.example.festival2048;
+
+import android.util.Log;
+import android.widget.Toast;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MiiStudioApi {
+    static final String TESTCODE =
+            "080240030803032c040e0308050409020e040a070001000804000a01001e4004000214031304080f06030b05030a";
+
+    private String miiCode;
+    private String miiUrl;
+
+    public MiiStudioApi(String miiCode) {
+        this.miiCode = miiCode;
+    }
+
+    public MiiStudioApi() {
+        this.miiCode = TESTCODE;
+    }
+
+    public ArrayList<Integer> codeToBytes(String code)
+    {
+        ArrayList<Integer> bytes = new ArrayList<>();
+        if (code.length() != TESTCODE.length())
+            throw new NumberFormatException();
+
+        bytes.add(0);
+        for (int i = 0; i < code.length(); i += 2) {
+            bytes.add(Integer.parseInt(code.substring(i, i+2), 16));
+        }
+
+        return bytes;
+    }
+
+    public String bytesToUrl(ArrayList<Integer> bytes)
+    {
+        String miiData = "";
+        int eo, i = 0;
+        int n = 256;
+
+        // https://github.com/HEYimHeroic/mii2studio/blob/74014fe199a358e809b2af2022a6eafaa7ec9931/mii2studio.py#L308
+        for (Integer v: bytes) {
+            eo = (7 + (v ^ n)) % 256;
+            n = eo;
+            String b = String.format("%1$02X", eo).toLowerCase(Locale.ROOT);
+            miiData += b;
+            Log.println(Log.DEBUG, "mii data decode:", "byte " + i + ": " + b);
+            i++;
+        }
+
+        String encodedData = new String(miiData.getBytes(StandardCharsets.US_ASCII), StandardCharsets.UTF_8);
+
+        Log.println(Log.DEBUG, "encoded mii data:", encodedData);
+        return "https://studio.mii.nintendo.com/miis/image.png?data=" + encodedData +
+                "&type=face&expression=normal&width=128&bgColor=FFFFFF00&clothesColor=default&cameraXRotate=0&cameraYRotate=0&cameraZRotate=0&characterXRotate=0&characterYRotate=0&characterZRotate=0&lightDirectionMode=none&instanceCount=1&instanceRotationMode=model";
+    }
+
+    public String getMiiCode() {
+        return miiCode;
+    }
+
+    public String getMiiUrl() {
+        if (miiUrl == null)
+        {
+            try {
+                miiUrl = bytesToUrl(codeToBytes(miiCode));
+            }
+            catch (NumberFormatException nfe)
+            {
+                throw nfe;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+        return miiUrl;
+    }
+}
